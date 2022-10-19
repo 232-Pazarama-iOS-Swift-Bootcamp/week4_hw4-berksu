3b08ba3eddb23af402f659498bd47067392f6fa2
berksu <berksukismet@gmail.com>
2022-10-19 21:22:50 +0300

    IQKeyboard added.

--- FlickerApiApp/App/Delegates/AppDelegate.swift
+++ FlickerApiApp/App/Delegates/AppDelegate.swift
@@ -14,10 +14,11 @@ import FirebaseFirestore
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         setupFirebase()
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
         return true
     }
     
